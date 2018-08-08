b37f3e282885185a0ec012002db17a7b29b5c8bd
Farheen <farheenzm@gmail.com>
2018-08-08 15:50:14 +0100

    Initial setup

---
+++ client/src/app.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+const appStart = function(){
+  console.log("It's working!");
+
+  const url = 'https://restcountries.eu/rest/v2/all'
+  makeRequest(url, requestComplete);
+
+};
+
+document.addEventListener('DOMContentLoaded', appStart);
---
+++ client/src/services/request.js
@@ -0,0 +1,7 @@
+const Request = function(url) {
+  this.url = url;
+}
+
+// get, post etc requests depending on the requirements
+
+module.exports = Request;
