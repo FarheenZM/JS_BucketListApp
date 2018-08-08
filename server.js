970ba7e51d71c63ad649add480b1537cf51edf1c
Richard Phillips-Kerr <r.phillips.kerr@gmail.com>
2018-08-08 16:40:28 +0100

    Clear all countries route complete

--- server.js
+++ server.js
@@ -15,6 +15,30 @@ MongoClient.connect('mongodb://localhost:27017', function(err, client) {
   }
   const db = client.db("cbdb");
 
+  server.get('/api/listcountries', function(req, res){
+    const listCountries = db.collection('listCountries');
+    listCountries.find().toArray(function(err, allListCountries){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.json(allListCountries);
+    })
+  })
+
+  server.delete('/api/listcountries', function(req,res) {
+    const filterObject = {};
+    const listCountries = db.collection('listCountries');
+    listCountries.deleteMany(filterObject, function(err, result) {
+      if(err)
+      res.status(500);
+      res.send()
+    })
+    res.status(204);
+    res.send();
+  })
+
   console.log('Connected to database');
 
   server.listen(3000, function(){
